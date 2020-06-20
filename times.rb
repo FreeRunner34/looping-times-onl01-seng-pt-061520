c46398fd8605ae1f7b3460f36c3ab8c1f0ab78d0
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-20 15:22:16 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,10 +1,8 @@
 counter = 0 
 def using_times
-	#counter = 0 
 		7.times do 
 	puts "Wingardium Leviosa" 
-	#counter = counter + 1 
- # puts "we practiced Wingardium Leviosa ${counter} times!"
+
 	end 
 
 
